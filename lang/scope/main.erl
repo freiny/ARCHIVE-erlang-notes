12f1314373097b10d926945a9dad8330c9eacd15
name <freiny50@gmail.com>
2016-09-12 12:54:50 -0500

    Update lang/scope: add comments on clauses

--- lang/scope/main.erl
+++ lang/scope/main.erl
@@ -7,6 +7,10 @@
 % the variable N is not being redefined
 % and can be a different value in a different function
 
+% Variables cannot "escape" outside the clause.
+% There's no such thing as global or private variables shared by
+% different clauses in the same function.
+
 fn1() ->
 	N=21,
 	io:format("~p~n",[N]).
