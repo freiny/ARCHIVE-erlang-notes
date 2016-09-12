1e8dbb8237a76d71bc048f8bc3aab8c5ecffda08
name <freiny50@gmail.com>
2016-09-12 15:07:32 -0500

    Add lang/types

---
+++ lang/types/main.erl
@@ -0,0 +1,37 @@
+-module(main).
+-export([init/0]).
+
+init() ->
+
+	% Variables are immutable, once set, they can't be changed
+	% N = 4,
+	% N = 5,
+	% ERRORS:
+	% file:line: Warning: no clause will ever match
+	% file:line: Warning: the guard for this clause evaluates to 'false'
+
+
+	% INTEGERS:
+	% Integer names must start with uppercase
+	A = 3,
+	B = 4,
+	Sum = A + B + 5,
+
+	io:format("~p ~n", [Sum]),
+	% OUTPUT: 12
+
+	% ATOMS:
+	% Atom names must start with lowercase
+	% Atoms are global and are similar to constants
+	% and enumerated types
+
+	io:format("~p ~p ~n", [option_1, option_x]),
+	% OUTPUT: option_1 option_x
+
+	OptA = option_1,
+	OptB = option@2,
+	OptC = 'Option_3',
+	io:format("~p ~p ~p ~n", [OptA, OptB, OptC]),
+	% OUTPUT: option_1 option@2
+
+	init:stop().
