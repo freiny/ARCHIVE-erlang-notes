f5846e4ea73fab7ebc43c05dcff05ded6281a08f
name <freiny50@gmail.com>
2016-09-13 12:02:50 -0500

    Update lang/modules: add comments for module and export

--- lang/modules/geometry.erl
+++ lang/modules/geometry.erl
@@ -1,7 +1,6 @@
--module(geometry).
--export([area/1]).
-
-% function overloading
+-module(geometry).	% geometry is the name of the module
+-export([area/1]).	% exported functions are accessible by outside modules
+% area/1 means the function area has 1 input parameter
 
 area({square, L}) -> L * L;
 area({rectangle, W, H}) -> W * H.
