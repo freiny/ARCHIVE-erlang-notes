ad8ff270830df782fe3630a1eaf3469ce9b6a682
name <freiny50@gmail.com>
2016-09-13 18:39:51 -0500

    Update lang/funs: use system map function

--- lang/funs/main.erl
+++ lang/funs/main.erl
@@ -2,29 +2,17 @@
 -export([init/0]).
 
 init() ->
-	Dbl = fun(N) -> io:format("~p ~p ~n", [N, N * 2]) end,
-	Sqr = fun(N) -> io:format("~p ~p ~n", [N, N * N]) end,
+	Dbl = fun(N) -> N * 2 end,
+	Sqr = fun(N) -> N * N end,
 
-	lib:map(Dbl, [1,2,3,4]),
-	% OUTPUT:
-	% 1 2
-	% 2 4
-	% 3 6
-	% 4 8
+	io:format("~p~n", [ lists:map(Dbl,[1,2,3,4]) ]),
+	% OUTPUT: [2,4,6,8]
 
-	lib:map(Sqr, [1,2,3,4]),
-	% OUTPUT:
-	% 1 1
-	% 2 4
-	% 3 9
-	% 4 16
+	io:format("~p~n", [ lists:map(Sqr,[1,2,3,4]) ]),
+	% OUTPUT: [1,4,9,16]
 
-	% IsEven = fun(N) ->
-	% 	even(0) -> true;
-	% 	even(1) -> false,
-	% 	Rem = N rem 2,
-	% 	even(Rem).
-	% io:format("~p~n", [isEven(5)]),
-
+	IsEven = fun(N) -> (N rem 2) =:= 0 end,
+	io:format("~p~n", [ lists:map(IsEven,[1,2,3,4]) ]),
+	% OUTPUT: [false,true,false,true]
 
 	init:stop().
