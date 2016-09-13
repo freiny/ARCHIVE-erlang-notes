56e68bb9ee9548d349f972076f24fca2a292f93c
name <freiny50@gmail.com>
2016-09-13 13:09:30 -0500

    Add lang/test: show how to do quick tests

---
+++ lang/test/geometry.erl
@@ -0,0 +1,16 @@
+-module(geometry).	% geometry is the name of the module
+-export([test/0]).
+-export([area/1]).	% exported functions are accessible by outside modules
+% area/1 means the function area has 1 input parameter
+% fnName/N , N is the arity of the function
+
+area({square, L}) -> L * L;
+area({rectangle, W, H}) -> W * H;
+area({circle, R}) -> 3.14 * R * R.
+
+test() ->
+	25 = area({square, 5}),
+	12 = area({rectangle, 3,4}),
+	50.24 = area({circle, 4}),
+	io:format("PASSED - ~p~n",[geometry]),
+	init:stop().
