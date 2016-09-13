24cef3a66f6869a52048e97106f5a1324a864b3d
name <freiny50@gmail.com>
2016-09-13 15:30:40 -0500

    Update lang/modules: simplify module example

--- lang/modules/main.erl
+++ lang/modules/main.erl
@@ -3,19 +3,13 @@
 
 init() ->
 
-	% Calling a function from another module
-	Shape = {square, 4},
-	Area = geometry:area(Shape),
-	io:format("SHAPE:~p~nAREA:~p~n", [Shape, Area]),
+	% Calling functions from different modules
+	modA:init(),
+	modB:init(),
+	modC:init(),
 	% OUTPUT:
-	% Shape:{square,4}
-	% AREA:16
-
-	Shape2 = {circle, 4},
-	Area2 = geometry:area(Shape2),
-	io:format("SHAPE:~p~nAREA:~p~n", [Shape2, Area2]),
-	% OUTPUT:
-	% Shape:{circle,4}
-	% AREA:50.24
+	% modA running...
+	% modB running...
+	% modC running...
 
 	init:stop().
