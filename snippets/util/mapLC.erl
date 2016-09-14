89967c0b1b71cbfbf5ffdc796756b9a0266b4f13
name <freiny50@gmail.com>
2016-09-14 15:02:20 -0500

    Update snippets/util: add mapLC: map function using list comprehensions

---
+++ snippets/util/mapLC.erl
@@ -0,0 +1,11 @@
+-module(mapLC).
+-export([init/0]).
+
+% Map function using List Comprehensions
+
+init() ->
+	Dbl = fun(X) -> 2*X end,
+	io:format("~p~n",[ mapLC(Dbl,[1,2,3,4]) ]),
+	init:stop().
+
+mapLC(Fn, L) -> ([Fn(X) || X <- L]).
