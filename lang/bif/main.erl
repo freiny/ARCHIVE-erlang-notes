3bc893bf3967dcd20b864cd7c293c89b2dc5e07f
name <freiny50@gmail.com>
2016-09-15 14:11:27 -0500

    Update lang/bif: add type tests and tuple/list length functions

--- lang/bif/main.erl
+++ lang/bif/main.erl
@@ -15,4 +15,23 @@ init() ->
 	io:format("~p~n",[T2]),
 	% OUTPUT: {{a,1},{b,2},{c,3}}
 
+	io:format("~p~n", [is_tuple(item)] ),
+	% OUTPUT: false
+	io:format("~p~n", [is_tuple({item, cake})] ),
+	% OUTPUT: true
+	io:format("~p~n", [tuple_size({item, cake})] ),
+	% OUTPUT: 2
+
+	io:format("~p~n", [is_list(item)] ),
+	% OUTPUT: false
+	io:format("~p~n", [is_list([1,2,3,4])] ),
+	% OUTPUT: true
+	io:format("~p~n", [length([1,2,3,4])] ),
+	% OUTPUT: 4
+
+	N = 4,
+	IsIntList = [is_integer(3), is_integer(3.14), is_integer(N), is_integer(item)],
+	io:format("~p~n", [IsIntList] ),
+	% OUTPUT: [true,false,true,false]
+
 	init:stop().
