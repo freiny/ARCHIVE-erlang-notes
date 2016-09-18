0b49914f3322e1a174ec8af4e0ce8b207f7df373
name <freiny50@gmail.com>
2016-09-18 18:41:28 -0500

    Update lang/concurrency ping: add output

--- lang/concurrency/ping/main.erl
+++ lang/concurrency/ping/main.erl
@@ -4,3 +4,7 @@
 init() ->
 	Pid = server:init(),
 	server:ping(Pid,{data,"helloworld"}).
+	% OUTPUT:
+	% ping() self(<0.3.0>) Pid(<0.57.0>) Req({data,"helloworld"})
+	% rpc() sent: self(<0.3.0>) Pid(<0.57.0>) Req({data,"helloworld"})
+	% listen() received: self(<0.57.0>) Pid(<0.3.0>) Req({data,"helloworld"})
